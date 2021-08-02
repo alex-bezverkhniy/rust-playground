f113847d9d5560b11228f4269fb51ad43e9e7131
Alexander Bezverkhniy <alexander.bezverkhniy@gmail.com>
2021-08-02 14:48:19 +0000

    ignore rust binaries

--- target/debug/deps/rust_playground-abc5366132fd938c.d
+++
@@ -1,5 +0,0 @@
-/home/runner/rust-playground/target/debug/deps/rust_playground-abc5366132fd938c: src/main.rs
-
-/home/runner/rust-playground/target/debug/deps/rust_playground-abc5366132fd938c.d: src/main.rs
-
-src/main.rs:
