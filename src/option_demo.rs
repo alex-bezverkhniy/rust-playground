21ca27bac7ab2b36d8001dabccc88b7b2b4ecec1
Alexander Bezverkhniy <alexander.bezverkhniy@gmail.com>
2021-08-04 13:45:11 +0000

    Option sample

--- src/option_demo.rs
+++ src/option_demo.rs
@@ -1,7 +1,11 @@
 use std::option::Option;
 
 fn safe_division(dividen: u32, division: u32) -> Option<u32> {
-  Some(0)
+  if division == 0 {
+    None
+  } else {
+    Some(dividen / division)
+  }
 }
 
 #[cfg(test)]
@@ -9,6 +13,18 @@ mod tests {
 
   #[test]
   fn test1() {
-    assert_eq!(0, super::safe_division(1,1).unwrap());
+    assert_eq!(1, super::safe_division(1,1).unwrap());
   }
+
+  #[test]
+  fn test_none() {
+    assert_eq!(None, super::safe_division(1,0));
+    assert_eq!(2, super::safe_division(4,2).unwrap());
+  }
+
+  #[test]
+  fn test2() {
+    assert_eq!(2, super::safe_division(4,2).unwrap());
+  }
+
 }
